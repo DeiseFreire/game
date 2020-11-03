f405ea1b893adbb6b4c89cb79ea1bd732c5efa61
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-03 19:13:34 -0300

    Update desenhar-o-jogo.js

--- desenhar-o-jogo.js
+++ desenhar-o-jogo.js
@@ -6,20 +6,21 @@ this.load.image('background','images/backGroundGame800x600.png');
 this.load.image('cloud','images/Cloud.png');
 //CLASSE GAME.js
 //FUNCTION CREATE
-// Adicionando nuvens a camada cloudLayer.this.cloudOne=this.add.sprite(0,0,'cloudOne'); 
-// Para desenhar as imagens de fundo a gente vai usar o objeto sprite 
-// O sprite é um objeto com várias funcionalidades não só de desenho
-// A gente vai usar o sprite para  desenhar todos os objetos 2D 
-// Ele atualiza automaticamente a visualização 
-// A gente não vai precisar usar a função render 
-// O sprite tem o update dele quando chega no laço principal é a hora de fazer o update 
+//Adicionando nuvens a camada cloudLayer.this.cloudOne=this.add.sprite(0,0,'cloudOne'); 
+//Para desenhar as imagens de fundo a gente vai usar o objeto sprite 
+//O sprite é um objeto com várias funcionalidades não só de desenho
+//A gente vai usar o sprite para  desenhar todos os objetos 2D 
+//Ele atualiza automaticamente a visualização 
+//A gente não vai precisar usar a função render 
+//O sprite tem o update dele quando chega no laço principal é a hora de fazer o update  
+//O body é o objeto que fica dentro do sprite que a gente vai usar para controlar a simulação física. A gente vai colocar, por exemplo, qual é a gravidade, a massa do sprite, do objeto que está representado pelo sprite 
 this.clo
 udLayer.add(this.cloudOne);
 //FUNCTION UPDATE
-// A gente chama o Update do sprite para atualizar o estado lógico do objeto associado ao sprite
-// Por exemplo, a gente tem um inimigo que enquanto ele não for vencido ele vai ficando mais forte. A gente pode programar isso no Update do sprite 
-// Isso pode ter alguma visualização depois, ou pode ser só o estado do jogo 
-// O input é como que o sprite responde às entradas 
+//A gente chama o Update do sprite para atualizar o estado lógico do objeto associado ao sprite
+//Por exemplo, a gente tem um inimigo que enquanto ele não for vencido ele vai ficando mais forte. A gente pode programar isso no Update do sprite 
+//Isso pode ter alguma visualização depois, ou pode ser só o estado do jogo 
+//O input é como que o sprite responde às entradas 
 //movimentando a nuvem
 this.cloudOne.x+=this.directionCloud;
 if(this.directionCloud>0){
